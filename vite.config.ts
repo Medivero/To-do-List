18e2bf9b312f2dd9edaa88eed5def68a7e3b56c9
Medivero <artembolyachinov@gmail.com>
2025-03-31 21:50:04 +0300

    Changes for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: 'https://github.com/Medivero/To-do-List',
+  base: '/To-do-List/',
 })
